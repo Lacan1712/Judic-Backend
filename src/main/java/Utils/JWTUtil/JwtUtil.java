5fe60cb0c904020e225b45d4eae36d2e69d8fdcf
rodrigo <lacan.xrl80@gmail.com>
2024-09-08 14:42:27 -0400

    feat: Implementando classe utilitária para criação de token jwt

--- src/main/java/Utils/JWTUtil/JwtUtil.java
+++ src/main/java/Utils/JWTUtil/JwtUtil.java
@@ -1,4 +1,40 @@
 package Utils.JWTUtil;
 
+
+import Utils.AlgorithmsUtil.RSALoadKeyUtil;
+import Utils.EnviromentUtil.EnvUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Date;
+import java.util.Map;
+
 public class JwtUtil {
+
+    RSAPrivateKey privateKey;
+    RSAPublicKey publicKey;
+
+    public JwtUtil() {
+        publicKey = (RSAPublicKey) RSALoadKeyUtil.loadPublicKey();
+        privateKey = (RSAPrivateKey) RSALoadKeyUtil.loadPrivateKey();
+    }
+
+    public String generateJWTWithClaims(Map<String, Object> claims) {
+        Algorithm algorithm = Algorithm.RSA256(publicKey, privateKey);
+
+        // Cria o token JWT
+        return JWT.create()
+                .withIssuer("judic-bakend")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 3600 * 1000)) // 1 hora de expiração
+                .withPayload(claims) // Adiciona as claims
+                .sign(algorithm);
+    }
+
+    public void getSubjectsFromToken(String token) {
+
+    }
+
 }
