2751c9052c925aaaac82b2471c7458d10b5cd985
Rodrigo Lacan <65981796+Lacan1712@users.noreply.github.com>
2024-09-13 18:14:31 -0400

    Revert "feat: Endpoint de auth usando os serviços necessários e retornando o token conforme as regras."

--- src/main/java/App/Controllers/AuthController.java
+++ src/main/java/App/Controllers/AuthController.java
@@ -11,12 +11,10 @@ import java.util.Optional;
 import Database.Entities.Usuarios;
 import Database.Services.UsuarioService;
 import Utils.AlgorithmsUtil.RSAKeyUtil;
+import Utils.EnviromentUtil.EnvUtil;
 import Utils.JWTUtil.JwtUtil;
 import Utils.PasswordHashingUtil.PasswordHashingUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -43,30 +41,10 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Object> getString(@RequestBody CredenciaisDTO credenciaisDTO) {
-        Map<String, Object> claims = new HashMap<>();
-        Map<String, Object> response = new HashMap<>();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        Optional<Usuarios> usuariosOptional = authUserService.logarUsuario(credenciaisDTO.username(), credenciaisDTO.password());
-
-        if(usuariosOptional.isEmpty()){
-            httpHeaders.add("WWW-Authenticate","Basic realm=\"Access to API\", charset=\"UTF-8\"");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                                 .headers(httpHeaders)
-                                 .body("Usuário não autorizado");
-        }
-
-
-        var usuarioLogado = usuariosOptional.get();
-        claims.put("id",usuarioLogado.getId());
-        claims.put("nome",usuarioLogado.getNome());
-        claims.put("email",usuarioLogado.getEmail());
-        claims.put("role",usuarioLogado.getRole());
-
-        var tokenJwt = jwtUtil.generateJWTWithClaims(claims);
-
-        response.put("token", tokenJwt);
-        return ResponseEntity.accepted().body(response);
+    public String getString(@RequestBody CredenciaisDTO credenciaisDTO) {
+        return authUserService.logarUsuario(credenciaisDTO.username(), credenciaisDTO.password())
+                .map(usuario -> "usuário logado: " + usuario.getNome())
+                .orElse("Usuário ou senha inválido");
     }
 
     @GetMapping("/users")
